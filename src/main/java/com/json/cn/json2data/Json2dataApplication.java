095d39d469fe64addd19d640649bfa87fcfed663
Alex67gg <15067079604@163.com>
2018-09-28 14:49:35 +0800

    init git

---
+++ src/main/java/com/json/cn/json2data/Json2dataApplication.java
@@ -0,0 +1,12 @@
+package com.json.cn.json2data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Json2dataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Json2dataApplication.class, args);
+	}
+}
